d665a8f6fc3b2141aa5842107e4212ef4e379af7
kwj5584 <kvvj5584@gmail.com>
2021-04-12 15:04:24 +0900

    API_KEY 상수화

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -2,5 +2,5 @@
 export const USER_SERVER = '/api/users';
 
 export const API_URL = 'https://api.themoviedb.org/3/';
-
-export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p';
+export const API_KEY = 'ae2ce85936743534db65d4e0eec7f115';
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
