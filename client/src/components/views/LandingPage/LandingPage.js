b523db7576ca816b58412fadce96144a6211a178
kwj5584 <kvvj5584@gmail.com>
2021-04-12 15:05:12 +0900

    메인이미지와 영화리스트 보여주는 컴포넌트

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,11 +1,56 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import { FaCode } from "react-icons/fa";
+import { API_KEY, API_URL, IMAGE_BASE_URL } from '../../Config';
+import axios from 'axios';
+import MainImage from './Sections/MainImage';
+import GridCards from '../commons/GridCards';
+import { Row } from 'antd';
 
 function LandingPage() {
+    const [Movies, setMovies] = useState([]);
+    const [MainMovieImage, setMainMovieImage] = useState(null);
+
+    useEffect(()=>{
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&alnguage=en-US&page=1`;
+
+        fetch(endpoint)
+        .then(response=>response.json())
+        .then(response=>{
+            setMovies([...response.results])
+            setMainMovieImage(response.results[0])
+        })
+        // axios.get(endpoint).then(res=>console.log('test:',res))
+    },[])
     return (
-        <>
-            
-        </>
+        <div style={{width:'100%', margin:'0'}}>
+            {MainMovieImage && 
+                <MainImage
+                    image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
+                    title={MainMovieImage.original_title}
+                    text={MainMovieImage.overview}    
+                />
+            }
+            <div style={{width:'85%', margin: '1rem auto'}}>
+                <h2>Movies by latest</h2>
+                <hr/>
+                <Row gutter={ [16,16] }>
+                    { Movies && Movies.map((movie, index) => (
+                        <React.Fragment key = {index}>
+                            <GridCards 
+                                image={movie.poster_path ? 
+                                    `${IMAGE_BASE_URL}w500${movie.poster_path}` : null}
+                                movieId={movie.id}
+                                movieName={movie.original_title}
+                            />
+                        </React.Fragment>
+                    ))}
+                </Row>
+            </div>
+            <div style={{display:'flex', justifyContent:'center'}}>
+                <button>Load More</button>
+            </div>
+
+        </div>
     )
 }
 
