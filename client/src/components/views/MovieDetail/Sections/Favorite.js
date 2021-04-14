9b4e55aa2bbbe43bc04d131875506d866bda6340
kwj5584 <kvvj5584@gmail.com>
2021-04-14 16:58:03 +0900

    Favorite컴포넌트 구현

---
+++ client/src/components/views/MovieDetail/Sections/Favorite.js
@@ -0,0 +1,44 @@
+import React,{ useEffect, useState} from 'react'
+import Axios from 'axios'
+
+function Favorite(props) {
+    const movieId = props.movieId;
+    const userFrom = props.userFrom;
+    const movieTitle = props.movieInfo.title;
+    const moviePost = props.movieInfo.backdrop_path;
+    const movieRunTime = props.movieInfo.runtime;
+
+    const [FavoriteNumber, setFavoriteNumber] = useState(0)
+    const [Favorited, setFavorited] = useState(false)
+    useEffect(() => {
+        let variables = {
+            userFrom,
+            movieId
+        }
+        Axios.post('/api/favorite/favoriteNumber', variables)
+            .then(res=>{
+                if(res.data.success){
+                    setFavoriteNumber(res.data.favoriteNumber)
+                } else{
+                    alert("숫자 정보를 가져오는데 실패했습니다.")
+                }
+            })
+
+        Axios.post('/api/favorite/favorited', variables)
+            .then(res=>{
+                if(res.data.success){
+                    setFavorited(res.data.favorited)
+                } else{
+                    alert("정보를 가져오는데 실패했습니다.")
+                }
+            })
+        
+    }, [])
+    return (
+        <div>
+            <button>{ Favorited? "Not Favorite" : "Add to Favorite"} {FavoriteNumber} </button>
+        </div>
+    )
+}
+
+export default Favorite
