eb0d4fe1180eaf31389b3c9065475ca5fba14401
kwj5584 <kvvj5584@gmail.com>
2021-05-01 15:14:14 +0900

    좋아하는 영화 목록 추좋아하는 영화 목록 추가 및 제거 api가 및 제거 api

--- server/routes/favorite.js
+++ server/routes/favorite.js
@@ -27,7 +27,7 @@ router.post('/removeFromFavorite',(req,res)=>{
     Favorite.findOneAndDelete({ movieId: req.body.movieId, userFrom : req.body.userFrom})
         .exec((err,doc)=>{
             if(err) return res.status(400).send(err);
-            res.status(200).json({success: true, doc})
+            res.status(200).json({success: true})
         })
 })
 
@@ -35,8 +35,27 @@ router.post('/addToFavorite',(req,res)=>{
     const favorite = new Favorite(req.body);
 
     favorite.save((err,doc) => {
-        if(err) return res.status(400)>send(err)
-        return res.status(200).json({ success: true })
+        if(err) return res.status(400).send(err)
+        return res.status(200).json({ success: true, doc })
     });
+    
 })
+
+router.post('/getFavoriteMovie',(req,res)=>{
+    
+    Favorite.find({'userFrom' : req.body.userFrom})
+        .exec((err,favorites) => {
+            if(err) return res.status(400).send(err)
+            return res.status(200).json({ success: true, favorites })
+        })
+})
+
+router.post('/removeFromFavorite',(req, res)=>{
+    Favorite.findOneAndDelete({movieId : req.body.movieId, userFrom: req.body.userFrom})
+        .exec((err,doc)=>{
+            if(err) return res.status(400).send(err)
+            return res.status(200).json({ success:true})
+        })
+})
+
 module.exports = router;
