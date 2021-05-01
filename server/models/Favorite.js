28f211cb8f9d61d1350a10a6889ebe6da340354a
kwj5584 <kvvj5584@gmail.com>
2021-05-01 15:13:21 +0900

    모델 오류 수정

--- server/models/Favorite.js
+++ server/models/Favorite.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const favoriteSchema = Schema({
-    useFrom:{
-        type: mongoose.Schema.Types.ObjectId,
+    userFrom:{
+        type: Schema.Types.ObjectId,
         ref:"User"
     },
     movieId:{
